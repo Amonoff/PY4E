02ed99ca8a0ffe430f65d6fafc01c8d3c5514680
Amon Ochuka <51824139+Amonoff@users.noreply.github.com>
2021-01-25 15:18:12 +0300

    Create 10.1.py

---
+++ 10.1.py
@@ -0,0 +1,28 @@
+'''Revise a previous program as follows: Read and parse the
+“From” lines and pull out the addresses from the line. Count the num-
+ber of messages from each person using a dictionary.
+After all the data has been read, print the person with the most commits
+by creating a list of (count, email) tuples from the dictionary. Then
+sort the list in reverse order and print out the person who has the most
+commits.'''
+
+file = input('Enter file name:')
+dict_addresses = dict()
+dict_list = list()
+try:
+    file = open(file)
+except:
+    print('File does not exist')
+
+for line in file:
+    words = line.split()
+    if not line.startswith('From ') : continue
+    else:
+        dict_addresses[words[1]] = dict_addresses.get(words[1], 0) + 1
+        
+for address, frequency in list(dict_addresses.items()):
+            dict_list.append((frequency, address))
+        
+dict_list.sort(reverse = True)
+                              
+print(dict_list)
